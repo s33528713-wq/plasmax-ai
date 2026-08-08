8812758b1ac87f981dc3174ab5615edc9d3bfe60
s33528713-wq <s33528713@gmail.com>
2026-08-08 16:35:09 +0530

    Add image selection

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 
 function scanHMI() {
     alert("Plasmax AI\n\nScan Feature Coming Soon");
+    document.getElementById("imageInput").addEventListener("change", function () {
+    alert("Image Selected Successfully");
+});
 }
